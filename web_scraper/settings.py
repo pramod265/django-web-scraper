5845951b0c1cc04e31dadb0f6940161811e72135
pramod gupta <pramod.gupta@digimkey.com>
2020-05-19 20:39:04 +0530

    Localhost server wsgi application issue resoved

--- web_scraper/settings.py
+++ web_scraper/settings.py
@@ -168,5 +168,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # prod_db  =  dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(prod_db)
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
